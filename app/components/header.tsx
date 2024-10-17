7bacf21f04c88400af07d6e8b49b5971dd5fb16a
Yohan <yohan-fmr@hotmail.com>
2024-10-17 16:39:02 +0200

    modifie header and section

---
+++ app/components/header.tsx
@@ -0,0 +1,10 @@
+export const Header = () => {
+    return (
+        <header>
+            <section>
+                <h1 className="text-4xl font-bold text-primary-foreground"></h1>
+                yohan.com
+            </section>
+        </header>
+    )
+}
---
+++ app/components/section.tsx
@@ -0,0 +1,13 @@
+import { cn } from "@/lib/utils";
+import { PropsWithChildren } from "react";
+
+export const section = (props: PropsWithChildren<{ className?: string }>) => {
+  return (
+    <section className={cn("max-w-3xl px-4 m-auto", props.className)}>
+      {props.children}
+    </section>
+  );
+};
+
+
+
