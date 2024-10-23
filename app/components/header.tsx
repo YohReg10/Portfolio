4346f9a1795ce2422a95c882fadf03c26c39640d
Yohan <yohan-fmr@hotmail.com>
2024-10-23 12:18:00 +0200

    add folder components\ui, add button.tsx file

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,10 +1,18 @@
-export const Header = () => {
+import { Button } from "@/components/ui/button";
+import { section as Section } from "./section";
+
+export const header = () => {
     return (
-        <header>
-            <section>
-                <h1 className="text-4xl font-bold text-primary-foreground"></h1>
+        <header className="sticky top-0">
+            <Section className="flex items-baseline">
+                <h1 className="text-4xl font-bold text-primary-foreground">
                 yohan.com
-            </section>
+                </h1>
+                <div className="flex-1"/>
+                <ul>
+                    <Button className="size-6"></Button>
+                </ul>
+            </Section>
         </header>
-    )
-}
+    );
+};
--- app/components/section.tsx
+++ app/components/section.tsx
@@ -1,7 +1,7 @@
 import { cn } from "@/lib/utils";
 import { PropsWithChildren } from "react";
 
-export const section = (props: PropsWithChildren<{ className?: string }>) => {
+export const section = (props: PropsWithChildren<{className?: string}>) => {
   return (
     <section className={cn("max-w-3xl px-4 m-auto", props.className)}>
       {props.children}
