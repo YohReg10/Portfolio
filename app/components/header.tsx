3c353eed62aff219ec2c64b5a8710086a670d119
Yohan <yohan-fmr@hotmail.com>
2024-10-23 18:03:18 +0200

    add Hero.tsx file and picture

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,7 +1,7 @@
 import { Button, buttonVariants } from "@/components/ui/button";
 import { Section } from "./section";
 import { GithubIcon } from "@/app/components/icons/GithubIcon";
-import { Linkedin } from "@/app/components/icons/Linkedin";
+import { Linkedin } from "@/app/components/icons/LinkedinIcon";
 import Link from "next/link";
 import { cn } from "@/lib/utils";
 
@@ -11,7 +11,7 @@ export const Header = () => {
             <Section className="flex items-baseline">
                 <h1 className="text-lg font-bold text-primary-foreground">yohan.com</h1>
                 <div className="flex-1" />
-                <ul>
+                <ul className="flex items-center gap-2">
                     <Link 
                     href="https://github.com/YohReg10"
                     className={cn(buttonVariants({variant: "outline"}), "size-6 p-0")}>
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 import { Header } from "./components/header";
+import { Hero } from "./components/Hero";
 
 export default function Home() {
   return (
     <main>
-      <Header>
-    
-      </Header>
+      <Header />
+      <Hero />
       
     
     </main>
