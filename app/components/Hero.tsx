3a3404bbd28b5eb39e19817cc0ddf7b41c3be9b9
Yohan <yohan-fmr@hotmail.com>
2024-10-30 17:03:45 +0100

    modifie className Section.tsx file, modifie style css in Hero.tsx file

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -3,19 +3,19 @@ import { Section } from "./section";
 export const Hero = () => {
     return (
         <Section className="flex max-lg:flex-col items-start">
-            <div className="flex-[2]">
-                <h2 className="font-caption text-6xl font-bold" style={{ color: '#d9b96d' }}>Yohan Regad</h2>
-                <h3 className="font-caption text-4xl font-bold" style={{ color: '#f5f5dc' }}>Student Web Developer</h3>
-                <h4 className="font-caption text-2xl font-bold" style={{ color: '#d9b96d', marginTop: '2rem' }}>Who I am?</h4>
+            <div className="flex-[2]" style={{ marginLeft: '8rem' }}> {/* Utilisation de style en ligne pour ajuster la marge */}
+                <h2 className="font-caption text-4xl font-bold" style={{ color: '#d9b96d' }}>Yohan Regad</h2>
+                <h3 className="font-caption text-3xl font-bold" style={{ color: '#f5f5dc' }}>Student Web Developer</h3>
+                <h4 className="font-caption text-xl font-bold" style={{ color: '#d9b96d', marginTop: '1rem' }}>Who I am?</h4>
 
-                <p className="font-serif text-xl" style={{ color: '#e0e0e0', lineHeight: '1.5', marginBottom: '1rem', textAlign: 'justify' }}>
+                <p className="font-serif text-lg" style={{ color: '#e0e0e0', lineHeight: '1.5', marginBottom: '1rem', textAlign: 'justify' }}>
                     My name is Yohan Regad👽 <br />
                     I am a development student who has completed my training and is looking for an internship to further enhance my skills. <br />
                     I specialize in back-end development because managing and manipulating data is what excites me the most!<br />
                     I am curious, determined, and a sunny person who loves to joke every day! 😄<br />
                     I consider myself a challenger because I love taking on challenges. That’s why I am always seeking new perspectives through stimulating and enriching projects. 💪 
                 </p>
-                <p className="font-serif text-xl" style={{ color: '#d9b96d', marginBottom: '0.5rem' }}>My passions are:</p>
+                <p className="font-serif text-lg" style={{ color: '#d9b96d', marginBottom: '0.5rem' }}>My passions are:</p>
                 <ul className="list-none mt-2" style={{ color: '#e0e0e0' }}>
                     <li>✈️ Traveling</li>
                     <li>🏋️‍♂️ Sports</li>
@@ -29,6 +29,7 @@ export const Hero = () => {
                     src="yohan_noir_blanc_bis2024.jpg"
                     className="w-full h-auto rounded-3xl"
                     alt="Yohan's portrait"
+                    style={{ maxWidth: '70%', height: 'auto' }} // Réduit la largeur de l'image à 70%
                 />
             </div>
         </Section>
