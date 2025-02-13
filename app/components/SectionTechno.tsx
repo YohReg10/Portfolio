e8e7619b59a75100489d838ef0420b8c946e924f
Yohan <yohan-fmr@hotmail.com>
2025-02-13 18:55:15 +0100

    add logo react in card

--- app/components/SectionTechno.tsx
+++ app/components/SectionTechno.tsx
@@ -5,30 +5,29 @@ import { Section } from "./Section";
 export const SectionTechno = () => {
     return (
         <Section className="flex flex-wrap gap-8 justify-between p-5 rounded-lg mt-20 mx-auto w-full">
-    <div className="card ">
-        <h3>React</h3>
-        <p>Blabla</p>
-    </div>
-    <div className="card ">
-        <h3>React</h3>
-        <p>Blabla</p>
-    </div>
-    <div className="card ">
-        <h3>React</h3>
-        <p>Blabla</p>
-    </div>
-    <div className="card ">
-        <h3>React</h3>
-        <p>Blabla</p>
-    </div>
-    <div className="card ">
-        <h3>React</h3>
-        <p>Blabla</p>
-    </div>
-    <div className="card ">
-        <h3>React</h3>
-        <p>Blabla</p>
-    </div>
+            <div className="card ">
+                <img src="react_img.svg" />
+            </div>
+            <div className="card ">
+                <h3>JavaScript</h3>
+                <p></p>
+            </div>
+            <div className="card ">
+                <h3>MySQL</h3>
+                <p></p>
+            </div>
+            <div className="card ">
+                <h3>Symfony</h3>
+                <p></p>
+            </div>
+            <div className="card ">
+                <h3>Figma</h3>
+                <p></p>
+            </div>
+            <div className="card ">
+                <h3>Github</h3>
+                <p></p>
+            </div>
 </Section>
 
     )
