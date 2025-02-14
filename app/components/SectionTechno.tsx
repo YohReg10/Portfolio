3e04a32746e8f9487e9678dbd73670e16232680c
Yohan <yohan-fmr@hotmail.com>
2025-02-14 17:33:09 +0100

    add fonc-caption + dark text + h3 + alt for all cards

--- app/components/SectionTechno.tsx
+++ app/components/SectionTechno.tsx
@@ -1,27 +1,31 @@
-// Section Techno
-
 import { Section } from "./Section";
 
 export const SectionTechno = () => {
     return (
         <Section className="flex flex-wrap gap-8 justify-between p-5 rounded-lg mt-20 mx-auto w-full">
             <div className="card ">
-                <img src="react_img.svg" />
+                <img src="react_img.svg" alt="Logo React" />
+                <h3 className="font-caption text-[18px] dark:text-[#f5f5dc] mb-24">React/NextJS</h3>
             </div>
             <div className="card ">
-                <img src="javascript_img.svg" /> 
+                <img src="javascript_img.svg" alt="Logo JavaScript" /> 
+                <h3 className="font-caption text-[18px] dark:text-[#f5f5dc] mb-24">JavaScript</h3>
             </div>
             <div className="card ">
-                <img src="mysql_img.svg" style={{ marginTop: "-10px", width: "125px", height: "125px" }} />
+                <img src="mysql_img.svg" alt="Logo MySQL" style={{ marginTop: "-10px", width: "125px", height: "125px" }} />
+                <h3 className="font-caption text-[18px] dark:text-[#f5f5dc] mb-24">MySQL</h3>
             </div>
             <div className="card ">
-                <img src="symfony_img.svg" />
+                <img src="symfony_img.svg" alt="Logo Symfony" />
+                <h3 className="font-caption text-[18px] dark:text-[#f5f5dc] mb-24">Symfony</h3>
             </div>
             <div className="card ">
-                <img src="figma_img.svg" />
+                <img src="figma_img.svg" alt="Logo Figma" />
+                <h3 className="font-caption text-[18px] dark:text-[#f5f5dc] mb-24">Figma</h3>
             </div>
             <div className="card ">
-                <img src="github_img.svg" />
+                <img src="github_img.svg" alt="Logo Github" />
+                <h3 className="font-caption text-[18px] dark:text-[#f5f5dc] mb-24">Github</h3>
             </div>
 </Section>
 
