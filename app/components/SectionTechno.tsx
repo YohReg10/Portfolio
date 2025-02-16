03b1d7e9baa04c720370c69072cde8d881e8baf4
Yohan <yohan-fmr@hotmail.com>
2025-02-16 16:13:48 +0100

    call .card in root and .dark for theme mode

--- app/components/SectionTechno.tsx
+++ app/components/SectionTechno.tsx
@@ -5,7 +5,7 @@ export const SectionTechno = () => {
         <Section className="flex flex-wrap gap-8 justify-between p-5 rounded-lg mt-32 mx-auto w-full">
             <div className="card">
                 <img src="react_img.svg" alt="Logo React" />
-                <h3 className="font-caption text-[18px] dark:text-[#f5f5dc] mb-24">React/NextJS</h3>
+                <h3 className="font-caption text-[18px]  mb-24">React/NextJS</h3>
             </div>
             <div className="card ">
                 <img src="javascript_img.svg" alt="Logo JavaScript" /> 
