dacada19a8e66e4973a0535b46fa7e202ef59cf0
Yohan <yohan-fmr@hotmail.com>
2025-02-18 15:00:46 +0100

    create SectionProjects.tsx and add import Section

---
+++ app/components/SectionProjects.tsx
@@ -0,0 +1,20 @@
+import { Section } from "./Section";
+
+export const SectionProjects = () => {
+    return(
+        <Section className="flex flex-wrap gap-8 justify-between p-5 rounded-lg mt-32 mx-auto w-[92%] bg-pink-700">
+
+
+
+
+
+
+
+
+
+
+
+        </Section>
+
+    )
+}
