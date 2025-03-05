3da0304d11b151990ad355aaeaed3ec3769bb49e
Yohan <yohan-fmr@hotmail.com>
2025-03-05 15:09:38 +0100

    add flex row in section and one div for projects and one div for description

--- app/components/SectionProjects.tsx
+++ app/components/SectionProjects.tsx
@@ -2,19 +2,19 @@ import { Section } from "./Section";
 
 export const SectionProjects = () => {
     return(
-        <Section className="flex flex-wrap gap-8 justify-between p-5 rounded-lg mt-32 mx-auto w-[92%] bg-pink-700">
+        <Section className="flex flex-row gap-8 justify-between p-5 rounded-lg mt-32 mx-auto w-[92%] bg-pink-700">
+            {/* Colonne de gauche : Projets */}
+            <div className="w-2/4">
+                <p>Blabla</p>
+            </div>
+    
+            {/* Description à droite */}
+            <div className="w-2/4">
+                <p>blabla</p>
+            </div>
 
-
-
-
-
-
-
-
-
-
-
+            
+            
         </Section>
-
     )
 }
