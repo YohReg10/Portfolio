57ffaa63b504de181264c3e86436a4a8b3a2dfcb
Yohan <yohan-fmr@hotmail.com>
2025-03-10 14:38:44 +0100

    add css style in SectionProjects + link github portfolio

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -4,36 +4,38 @@ import { Section } from "./Section";
 
 export const Hero = () => {
     return (
-        <Section className="flex justify-between items-start max-lg:flex-col mt-20 mx-auto">
-    <div className="flex-[2] ml-20">
-        <h2 className="font-caption text-4xl font-bold text-[#e3ba59] dark:text-[#d9b96d]">Yohan Regad</h2>
-        <h3 className="font-caption text-3xl font-bold text-[#] dark:text-[#f5f5dc]">Student Web Developer</h3>
-        <h4 className="font-caption text-xl font-bold text-[#e3ba59] dark:text-[#d9b96d]">Who I am?</h4>
+        <Section className="flex justify-between items-start max-lg:flex-col mt-20 mx-auto" aria-labelledby="hero-title">
+            <div className="flex-[2] ml-20">
+                <h1 id="hero-title" className="font-caption text-4xl font-bold text-[#e3ba59] dark:text-[#d9b96d]">Yohan Regad</h1>
+                <h2 className="font-caption text-3xl font-bold dark:text-[#f5f5dc]">Student Web Developer</h2>
+                <h3 className="font-caption text-xl font-bold text-[#e3ba59] dark:text-[#d9b96d]">Who I am?</h3>
 
-        <p className="font-serif text-lg leading-relaxed mb-4 text-justify text-[#333333] dark:text-[#e0e0e0]">
-            My name is Yohan Regad👽 <br />
-            I am a development student who has completed my training and is looking for an internship to further enhance my skills.<br />
-            I specialize in back-end development because managing and manipulating data is what excites me the most!<br />
-            I am curious, determined, and a sunny person who loves to joke every day! 😄<br />
-            I consider myself a challenger because I love taking on challenges. That’s why I am always seeking new perspectives through stimulating and enriching projects. 💪 
-        </p>
-        <p className="font-serif text-lg mb-2 text-[#e3ba59] dark:text-[#d9b96d]">My passions are:</p>
-        <ul className="list-none mt-2 text-[#e3ba59] dark:text-[#d9b96d]">
-            <li>✈️ Traveling</li>
-            <li>🏋️‍♂️ Sports</li>
-            <li>🎧 Music (nothing better for escaping for a moment)</li>
-            <li>🕹️ Video games</li>
-        </ul>
-        <span className="italic mt-4 block text-[#e3ba59] dark:text-[#d9b96d]" > "When you think you can't, try once more."</span> - Japanese proverb 🍀
-    </div>
-    <div className="flex-1 mt-0">
-        <img 
-            src="yohan_noir_blanc_bis2024.jpg"
-            className="h-auto rounded-3xl border-2 border-[#e3ba59] mx-auto w-[75%]"
-            alt="Yohan's portrait"
-            style={{ maxWidth: '90%', height: 'auto' }}
-        />
-    </div>
-</Section>
+                <p className="font-serif text-lg leading-relaxed mb-4 text-justify text-[#333333] dark:text-[#e0e0e0]">
+                    My name is Yohan Regad 👽 <br />
+                    I am a development student who has completed my training and is looking for an internship to further enhance my skills.<br />
+                    I specialize in back-end development because managing and manipulating data is what excites me the most!<br />
+                    I am curious, determined, and a sunny person who loves to joke every day! 😄<br />
+                    I consider myself a challenger because I love taking on challenges. That’s why I am always seeking new perspectives through stimulating and enriching projects. 💪 
+                </p>
+                <p className="font-serif text-lg mb-2 text-[#e3ba59] dark:text-[#d9b96d]">My passions are:</p>
+                <ul className="list-none mt-2 text-[#e3ba59] dark:text-[#d9b96d]">
+                    <li>✈️ Traveling</li>
+                    <li>🏋️‍♂️ Sports</li>
+                    <li>🎧 Music (nothing better for escaping for a moment)</li>
+                    <li>🕹️ Video games</li>
+                </ul>
+                <blockquote className="italic mt-4 block text-[#e3ba59] dark:text-[#d9b96d]" aria-label="Japanese proverb">
+                    "When you think you can't, try once more." - Japanese proverb 🍀
+                </blockquote>
+            </div>
+            <div className="flex-1 mt-0">
+                <img 
+                    src="yohan_noir_blanc_bis2024.jpg"
+                    className="h-auto rounded-3xl border-2 border-[#e3ba59] mx-auto w-[75%]"
+                    alt="Yohan's portrait in black and white with a confident smile"
+                    style={{ maxWidth: '90%', height: 'auto' }}
+                />
+            </div>
+        </Section>
     );
 };
--- app/components/SectionProjects.tsx
+++ app/components/SectionProjects.tsx
@@ -1,30 +1,44 @@
 import { Section } from "./Section";
 
 export const SectionProjects = () => {
-    return(
+    return (
         <Section className="flex flex-row gap-8 justify-between p-5 rounded-lg mt-32 mx-auto w-[92%]">
             
             {/* Colonne de gauche : Projets */}
             <div className="w-2/4">
-                <a href="https://github.com/YohReg10/Portfolio" target="_blank" rel="noopener noreferrer">
-                    <img src="Portfolio-Desk-620x410.jpg" className="h-auto rounded-3xl mx-auto w-[70%] ml-0" alt="Image d'ordinateur pour le projet Portfolio" />
-                </a>
-
+                <div className="relative">
+                    <img
+                        src="Portfolio-Desk-620x410.jpg"
+                        alt="Yohan's portfolio preview"
+                        className="h-auto rounded-3xl mx-auto w-[100%] ml-0 hover:opacity-90 transition-opacity"
+                        loading="lazy"
+                    />
+                    {/* Span avec le lien autour du texte */}
+                    <a
+                        href="https://github.com/YohReg10/Portfolio"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="inline-block bg-[#F1C40F] text-[#2C3E50] w-full text-center px-4 py-2 rounded-md text-sm font-medium absolute bottom-0 left-0 transition-colors"
+                    >
+                        👀 View Project
+                    </a>                
+                </div>
             </div>
     
             {/* Description à droite */}
-
             <div className="w-2/4 mt-0 mr-24">
                 <h2 className="font-serif text-3xl mb-2 font-bolt text-[#e3ba59] dark:text-[#d9b96d]">
                     Made with React ⚛️, TypeScript 💪, Tailwind CSS 🎨 <br />
                 </h2>
                 <p className="font-serif text-xl leading-relaxed mb-4 text-justify text-[#333333] dark:text-[#e0e0e0]">
-                    Interactive portfolio designed to showcase my projects, technical skills, and developer identity. Built to provide smooth and responsive navigation, it combines modernity, dynamism, and subtle animations to create an immersive user experience. <br />                </p>
+                    Interactive portfolio designed to showcase my projects, technical skills, and developer identity. Built to provide smooth and responsive navigation, it combines modernity, dynamism, and subtle animations to create an immersive user experience. <br />                
+                </p>
                 <p className="font-serif text-xl leading-relaxed mb-4 text-justify text-[#333333] dark:text-[#e0e0e0]">
                     ✨ A project crafted with care, passion, and a touch of love for detail... 👽<br />                
                 </p>
             </div>         
-            
         </Section>
-    )
-}
+    );
+};
+
+
