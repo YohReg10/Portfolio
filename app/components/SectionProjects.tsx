e75bd7d7786284b1b91512a548afcce61483ad5b
Yohan <yohan-fmr@hotmail.com>
2025-03-11 11:58:55 +0100

    organize listProjects

--- app/components/SectionProjects.tsx
+++ app/components/SectionProjects.tsx
@@ -1,9 +1,36 @@
 import { Section } from "./Section";
 
 const listProjects = [
-    {imgSrc : "Portfolio-Desk-620x410.jpg", alt : "Yohan's portfolio preview", href : "https://github.com/YohReg10/Portfolio", textTechno : "Made with React ⚛️, TypeScript 💪, Tailwind CSS 🎨", textDescription : "Interactive portfolio designed to showcase my projects, technical skills, and developer identity. Built to provide smooth and responsive navigation, it combines modernity, dynamism, and subtle animations to create an immersive user experience.", textAll : "✨ A project crafted with care, passion, and a touch of love for detail... 👽"},
-    {imgSrc : "ProjetTestTechnique.jpg", alt : "Test Technique enginer motor", href : "https://github.com/YohReg10/test-alternant", textTechno : "Made with React ⚛️, TypeScript 💪, Tailwind CSS 🎨", textDescription : "Un moteur de recherche d’activités à Montpellier, avec une interface fluide permettant de trouver facilement des aventures en intérieur et extérieur", textAll : "✨ A project crafted with care, passion, and a touch of love for detail... 👽"},
-    {imgSrc : "ProjetChavigny.jpg", alt : "Projet booking rooms", href : "https://github.com/SCollinPerso/PrivateChavigny", textTechno : "Made with Symfony, JavaScript, CSS", textDescription : "Une plateforme de réservation pour la ferme familiale, permettant à chacun d’accéder à son espace personnel, de voir les disponibilités des chambres, de réserver et de savoir qui est présent grâce à un calendrier intégré.", textAll : "✨ A project crafted with care, passion, and a touch of love for detail... 👽"},
+    {
+        imgSrc: "Portfolio-Desk-620x410.jpg",
+        alt: "Yohan's portfolio preview",
+        href: "https://github.com/YohReg10/Portfolio",
+        textTechno: "Made with React ⚛️, TypeScript 💪, Tailwind CSS 🎨",
+        textDescription:
+            "Interactive portfolio designed to showcase my projects, technical skills, and developer identity. "
+            + "Built to provide smooth and responsive navigation, it combines modernity, dynamism, and subtle animations "
+            + "to create an immersive user experience.",
+        textAll: "✨ A personal digital journey, reflecting my skills and vision, crafted with care and attention to detail. 👽"
+    },
+    {
+        imgSrc: "ProjetTestTechnique.jpg",
+        alt: "Test Technique engine motor",
+        href: "https://github.com/YohReg10/test-alternant",
+        textTechno: "Made with React ⚛️, TypeScript 💪, Tailwind CSS 🎨",
+        textDescription:
+            "A search engine for activities in Montpellier, with a smooth interface allowing users to easily find indoor and outdoor adventures.",
+        textAll: "✨ A project that brings local adventures to life, created with precision and passion for a seamless user experience. 👽"
+    },
+    {
+        imgSrc: "ProjetChavigny.jpg",
+        alt: "Projet booking rooms",
+        href: "https://github.com/SCollinPerso/PrivateChavigny",
+        textTechno: "Made with Symfony, JavaScript, CSS",
+        textDescription:
+            "A booking platform for the family house, allowing each user to access their personal space, view room availability,"
+            + "make reservations, and see who is present through an integrated calendar.",
+        textAll: "✨ Built with care to simplify bookings and enhance the experience of both guests and hosts. 👽"
+    }
 ];
 
 export const SectionProjects = () => {
@@ -12,29 +39,25 @@ export const SectionProjects = () => {
             {listProjects.map((projects) => (
                 <div key={projects.href} className="flex flex-row items-center gap-8">
 
-                    {/* Image à gauche */}
                     <div className="w-1/3">
                         <div className="relative">
                             <img
                                 src={projects.imgSrc}
                                 alt={projects.alt}
-                                className="h-auto rounded-3xl border-2 border-[#e3ba59] mx-auto w-[100%] ml-0 hover:opacity-90 transition-opacity"
-                                loading="lazy"
+                                className="w-[575px] h-[382px] object-cover rounded-3xl border-2 border-[#e3ba59] mx-auto hover:opacity-90 transition-opacity"                                loading="lazy"
                             />
 
-                            {/* Span avec le lien autour du texte */}
                             <a
                                 href={projects.href}
                                 target="_blank"
                                 rel="noopener noreferrer"
-                                className="inline-block bg-[#F1C40F] text-[#2C3E50] w-full text-center px-4 py-2 rounded-md text-sm font-medium absolute bottom-0 left-0 transition-colors"
+                                className="inline-block bg-[#F1C40F] text-[#2C3E50] w-full text-center px-4 py-2 rounded-3xl text-sm font-medium absolute bottom-0 left-0 transition-colors"
                             >
                                 👀 View Project
                             </a> 
                         </div>               
                     </div>
 
-                        {/* Description à droite */}
                     <div className="w-2/4 mt-0 mr-24">
                         <h2 className="font-serif text-3xl mb-2 font-bolt text-[#e3ba59] dark:text-[#d9b96d]">
                             {projects.textTechno}
