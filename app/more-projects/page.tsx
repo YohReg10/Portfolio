bae3d5b673187723b143cd6f7e91e5558a8e5b00
Yohan <yohan-fmr@hotmail.com>
2025-03-12 16:02:22 +0100

    import section in more-projects

--- app/more-projects/page.tsx
+++ app/more-projects/page.tsx
@@ -1,12 +1,16 @@
 // Page de Projets
 
-function MoreProjects() {
-    return (
-      <div>
-        <h1>Projets supplémentaires</h1>
-      
-      </div>
-    );
-  }
+import { Section } from "../components/Section";
+
+ const MoreProjects = () => {
+  return (
+    <Section className="flex flex-wrap lg:flex-nowrap gap-8 justify-between p-5 rounded-lg mt-32 mx-auto w-[92.5%]">
+      <h1>Projets supplémentaires</h1>
+
+    </Section>
+  );
+
+};
+
+export default MoreProjects
   
-  export default MoreProjects;
