6737e4db18f3221554296340c8dd0b9f4e9101e0
Yohan <yohan-fmr@hotmail.com>
2025-03-13 14:15:39 +0100

    add pico8.png + section for projects + created button retour au portoflio

--- app/more-projects/page.tsx
+++ app/more-projects/page.tsx
@@ -1,16 +1,37 @@
-// Page de Projets
+// MoreProjects.tsx
 
-import { Section } from "../components/Section";
+import Link from "next/link";
+import {Section} from "../components/Section"; // Assurez-vous d'utiliser l'importation par défaut
 
- const MoreProjects = () => {
+const MoreProjects = () => {
   return (
-    <Section className="flex flex-wrap lg:flex-nowrap gap-8 justify-between p-5 rounded-lg mt-32 mx-auto w-[92.5%]">
-      <h1>Projets supplémentaires</h1>
+    <div>
+      <div className="text-center mt-12">
+        <Link href="/" passHref>
+          <button className="bg-[#F1C40F] text-[#2C3E50] py-2 px-6 rounded-md text-lg">
+            Retour au portfolio
+          </button>
+        </Link>
+      </div>
 
-    </Section>
+      <h1 className="text-4xl font-bold text-center text-[#F1C40F]">🚀 Projets à venir</h1>
+      <p className="text-lg text-gray-500 text-center mt-2">
+        Voici quelques idées de projets en préparation. Stay tuned !
+      </p>
+
+      <Section className="flex flex-wrap lg:flex-nowrap gap-8 justify-between p-5 rounded-lg mt-32 mx-auto w-1/3">
+        <div className="flex flex-col items-center">
+          <img src="pico8.jpg" width="1000" height="1000" />
+        </div>
+        <div className="flex flex-col items-center">
+          <img src="pico8.jpg" />
+        </div>
+        <div className="flex flex-col items-center">
+          <img src="pico8.jpg" />
+        </div>
+      </Section>
+    </div>
   );
-
 };
 
-export default MoreProjects
-  
+export default MoreProjects;
